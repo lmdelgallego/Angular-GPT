e4f5fea835a5a4d4fe6d34f50ab85d367c57a0c9
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2025-06-22 13:12:06 -0500

    feat: add markdown support to chat messages
    - Added `marked` and `ngx-markdown` dependencies to package.json.
    - Updated app.config.ts to include `provideMarkdown` in the application providers.
    - Modified chatMessage.component.html to render message text using the `<markdown>` component.
    - Updated chatMessage.component.ts to import `MarkdownModule` for markdown rendering.

--- src/app/presentation/components/chat-bubbles/chatMessage/chatMessage.component.ts
+++ src/app/presentation/components/chat-bubbles/chatMessage/chatMessage.component.ts
@@ -1,17 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
 
-@Component( {
+@Component({
   selector: 'app-chat-message',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [MarkdownModule],
   templateUrl: './chatMessage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-} )
+})
 export class ChatMessageComponent {
-
   @Input({ required: true }) text!: string;
-
 }
